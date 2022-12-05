c78c6ac9a5d8c78b4fd6be24f6779980414663cb
Giga Amiridze <flameyungoxx@gmail.com>
2022-12-05 19:43:36 +0400

    Added module options in configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,20 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: './src/index.html'
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }
